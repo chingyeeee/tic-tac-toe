5040b80d45a5d0fbfe4651e8b7f0805fc79eb898
ronnie <ronnieyeung@fontrip.com>
2022-09-01 11:09:55 +0800

    完成遊戲

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function Square() {
-  return <button className="square">{/* TODO */}</button>;
+//component格子
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
+//component棋盤
 function Board() {
-  const status = "Next player: X";
+  //格子狀態
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
 
+  //渲染格子
   const renderSquare = function (i) {
-    return <Square />;
+    return <Square value={state.squares[i]} onClick={() => handleClick(i)} />;
   };
+
+  //變更格子狀態
+  const handleClick = (i) => {
+    const squares = state.squares.slice(); //複製原有的state
+    if (calcWinner(squares) || squares[i]) return;
+    squares[i] = state.xIsNext ? "X" : "O";
+    setState({ squares: squares, xIsNext: !state.xIsNext });
+  };
+
+  //calcWinner function
+  function calcWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calcWinner(state.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (state.xIsNext ? "X" : "O");
+  }
 
   return (
     <div>
@@ -35,6 +87,7 @@ function Board() {
   );
 }
 
+//component遊戲
 function Game() {
   return (
     <div className="game">
