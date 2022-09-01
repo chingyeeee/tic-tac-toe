b099a345ffc2fbfa01b63bdd3eba72aab5edbd96
ronnie <ronnieyeung@fontrip.com>
2022-09-01 16:23:16 +0800

    新增play again按鈕

--- src/index.js
+++ src/index.js
@@ -2,39 +2,65 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-//component格子
-function Square(props) {
+function Square({ value, onClick }) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={onClick}>
+      {value}
     </button>
   );
 }
 
-//component棋盤
-function Board() {
-  //格子狀態
-  const [state, setState] = useState({
-    squares: Array(9).fill(null),
-    xIsNext: true,
-  });
+function Restart({ onClick }) {
+  return (
+    <button className="restart" onClick={onClick}>
+      Play again
+    </button>
+  );
+}
 
-  //渲染格子
-  const renderSquare = function (i) {
-    return <Square value={state.squares[i]} onClick={() => handleClick(i)} />;
+function Game() {
+  const initialBoard = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialBoard);
+  const [isXNext, setIsXNext] = useState(true);
+  const nextSymbol = isXNext ? "X" : "O";
+  const winner = calculateWinner(squares);
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextSymbol;
+    }
+  }
+
+  const handleTurns = function (i) {
+    if (squares[i] != null || winner != null) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = nextSymbol;
+    setSquares(nextSquares);
+
+    setIsXNext(!isXNext);
   };
 
-  //變更格子狀態
-  const handleClick = (i) => {
-    const squares = state.squares.slice(); //複製原有的state
-    if (calcWinner(squares) || squares[i]) return;
-    squares[i] = state.xIsNext ? "X" : "O";
-    setState({ squares: squares, xIsNext: !state.xIsNext });
+  const handleStart = function () {
+    setSquares(initialBoard);
+    setIsXNext(true);
   };
 
-  //calcWinner function
-  function calcWinner(squares) {
-    const lines = [
+  function renderSquare(i) {
+    return <Square value={squares[i]} onClick={() => handleTurns(i)} />;
+  }
+
+  function renderRestartButton() {
+    return <Restart onClick={() => handleStart()} />;
+  }
+
+  function calculateWinner(squares) {
+    const possibleLines = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -44,8 +70,9 @@ function Board() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
+    // go over all possibly winning lines and check if they consist of only X's/only O's
+    for (let i = 0; i < possibleLines.length; i++) {
+      const [a, b, c] = possibleLines[i];
       if (
         squares[a] &&
         squares[a] === squares[b] &&
@@ -57,52 +84,40 @@ function Board() {
     return null;
   }
 
-  const winner = calcWinner(state.squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (state.xIsNext ? "X" : "O");
+  function isBoardFull(squares) {
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        return false;
+      }
+    }
+    return true;
   }
 
   return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
-}
-
-//component遊戲
-function Game() {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
       </div>
     </div>
   );
 }
-
-// ========================================
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
